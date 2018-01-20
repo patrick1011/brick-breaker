026b2b691fb4174bd5bc6448d2c938b0ef0bfbc3
Patrick Olden <patrickolden@googlemail.com>
2018-01-20 17:59:22 +0000

    paddle moving

---
+++ src/brickBreaker/Main.java
@@ -0,0 +1,23 @@
+package brickBreaker;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		Gameplay gameplay = new Gameplay();
+		
+		frame.setBounds(10, 10, 700, 600);
+		frame.setTitle("Breakout Ball");
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.add(gameplay);
+		frame.setVisible(true);
+	}
+
+}
